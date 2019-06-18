0c6a9881af1bc701b79edcd129ee82337b3d964b
pitts-michael <pittsm1@aetna.com>
2019-06-18 09:02:57 -0400

    [repo] adding more parameters

--- repo.tf
+++ repo.tf
@@ -1,5 +1,19 @@
 resource "github_repo" "this" {
-  name            = "${var.name}"
-  description     = "${var.description}"
-  homepage_url    = "${var.homepage_url}"
+  name                = "${var.name}"
+  description         = "${var.description}"
+  homepage_url        = "${var.homepage_url}"
+
+  private             = "${var.private}"
+  auto_init           = "${var.auto_init}"
+
+  has_issues          = "${var.has_issues}"
+  has_projects        = "${var.has_projects}"
+  has_wiki            = "${var.has_wiki}"
+  has_downloads       = "${var.has_downloads}"
+
+  allow_merge_commit  = "${var.allow_merge_commit}"
+  allow_squash_merge  = "${var.allow_squash_merge}"
+  allow_rebase_merge  = "${var.allow_rebase_merge}"
+
+
 }
